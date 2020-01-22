909c6d068c6fc438f552e22b24d5c6d17de97a06
Nii Yeboah <nii@vaadin.com>
2020-01-22 14:31:54 +0200

    Add graph setup

--- src/dagre-d3-component.js
+++ src/dagre-d3-component.js
@@ -1,15 +1,8 @@
-/**
- * @license
- * Copyright (C) 2015 Vaadin Ltd.
- * This program is available under Commercial Vaadin Add-On License 3.0 (CVALv3).
- * See the file LICENSE.md distributed with this software for more information about licensing.
- * See [the website]{@link https://vaadin.com/license/cval-3} for the complete license.
- */
-
 import { html, PolymerElement } from '@polymer/polymer/polymer-element';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin';
 import { ElementMixin } from '@vaadin/vaadin-element-mixin';
-import '@vaadin/vaadin-license-checker/vaadin-license-checker';
+import * as d3 from 'd3';
+import 'dagre-d3/dist/dagre-d3';
 
 /**
  * `<dagre-d3-component>` Web Component wrapper for Dagre D3
@@ -24,19 +17,14 @@ import '@vaadin/vaadin-license-checker/vaadin-license-checker';
  *
  * Custom property | Description | Default
  * ----------------|-------------|-------------
- * `--dagre-d3-component-property` | Example custom property | `unset`
+ * `--dagre-d3-width` | Width of SVG | `500px`
+ * `--dagre-d3-height` | Height of SVG | `500px`
  *
  * The following shadow DOM parts are available for styling:
  *
  * Part name | Description
  * ----------------|----------------
- * `part` | Example part
- *
- * The following state attributes are available for styling:
- *
- * Attribute    | Description | Part name
- * -------------|-------------|------------
- * `attribute` | Example styling attribute | :host
+ * `svg` | SVG element used for rendering the graph
  *
  * @memberof Vaadin
  * @mixes ElementMixin
@@ -49,8 +37,20 @@ class DagreD3Component extends ElementMixin(ThemableMixin(PolymerElement)) {
       <style>
         :host {
           display: block;
+
+          --dagre-d3-width: 500px;
+          --dagre-d3-height: 500px;
+        }
+        svg {
+          width: var(--dagre-d3-width);
+          height: var(--dagre-d3-height);
+          overflow: hidden;
         }
       </style>
+
+      <div class="card">
+        <svg id="svg"><g id="inner" /></svg>
+      </div>
     `;
   }
 
@@ -66,17 +66,38 @@ class DagreD3Component extends ElementMixin(ThemableMixin(PolymerElement)) {
     return {};
   }
 
-  /**
-   * @protected
-   */
-  static _finalizeClass() {
-    super._finalizeClass();
+  ready() {
+    super.ready();
+    const svg = d3.select(this.$.svg);
+    const inner = d3.select(this.$.inner);
+    const zoom = d3.zoom().on('zoom', () => {
+      inner.attr('transform', d3.event.transform);
+    });
 
-    const devModeCallback = window.Vaadin.developmentModeCallback;
-    const licenseChecker = devModeCallback && devModeCallback['vaadin-license-checker'];
-    if (typeof licenseChecker === 'function') {
-      licenseChecker(DagreD3Component);
+    svg.call(zoom);
+
+    // Create and configure the renderer
+    const render = dagreD3.render();
+
+    // Create the input graph
+    const g = new dagreD3.graphlib.Graph().setGraph({});
+
+    // Add nodes and edges
+    // json.nodes.forEach(node => g.setNode(node.id, { ...node }));
+    // json.edges.forEach(edge => g.setEdge(edge.from, edge.to, { ...edge }));
+
+    // Set margins, if not present
+    if (!g.graph().hasOwnProperty('marginx') && !g.graph().hasOwnProperty('marginy')) {
+      g.graph().marginx = 20;
+      g.graph().marginy = 20;
     }
+
+    g.graph().transition = selection => {
+      return selection.transition().duration(500);
+    };
+
+    // Render the graph into svg g
+    d3.select(this.$.inner).call(render, g);
   }
 }
 
